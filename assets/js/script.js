40e5b148ac6f92e3a9a46ae663dc65d6afdf0759
manbeltran <jbelt005@ucr.edu>
2021-11-16 21:33:50 -0800

    completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,71 @@
 // TODO: Declare any global variables we need
+let tails = document.querySelector('#tails')
+let heads = document.querySelector('#heads')
+let headsPercent = document.querySelector('#heads-percent')
+let tailsPercent = document.querySelector('#tails-percent')
+let tailsNumber = 0
+let headsNumber = 0
+let headsPercentNumber = 0
+let tailsPercentNumber = 0
+let flipTotal
+let clearButton = document.querySelector('#clear')
+//= headsNumber + tailsNumber
+
+let side = document.querySelector('#pennyImage')
+let flipButton = document.querySelector('#flip')
+//let rolledValue = Math.ceil(Math.round(Math.random() * 1))
+
+
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+
+    flipButton.addEventListener('click', function() {
+        let rolledValue = Math.ceil(Math.round(Math.random() * 1))
+        
+        if (rolledValue == 0) {
+            document.querySelector('#pennyImage').src = 'assets/images/penny-tails.jpg'
+            document.querySelector('#message').textContent = 'Tails'
+            document.getElementById('tails').innerHTML = tailsNumber = tailsNumber + 1
+            flipTotal = headsNumber + tailsNumber
+            //tailsPercentNumber = flipTotal / tailsNumber
+            //tailsPercent.textContent = tailsPercentNumber
+
+            //document.getElementById('tails-percent').innerHTML = Math.round((tailsNumber / flipTotal) * 100)
+        } else {
+            document.querySelector('#pennyImage').src = 'assets/images/penny-heads.jpg'
+            document.querySelector('#message').textContent = 'Heads'
+            document.getElementById('heads').innerHTML = headsNumber = headsNumber + 1
+            flipTotal = headsNumber + tailsNumber
+            //headsPercentNumber = flipTotal / headsNumber
+            //headsPercent.textContent = headsPercentNumber
+            //document.getElementById('heads-percent').innerHTML = Math.round((headsNumber / flipTotal) * 100)
+            
+        }
+        
+        tailsPercentNumber = flipTotal / tailsNumber
+        headsPercentNumber = flipTotal / headsNumber
+        document.getElementById('heads-percent').innerHTML = Math.round((headsNumber / flipTotal) * 100) + "%"
+        document.getElementById('tails-percent').innerHTML = Math.round((tailsNumber / flipTotal) * 100) + "%"
+        console.log(rolledValue)
+      
+        
+
+        clearButton.addEventListener('click', function () {
+            tailsNumber = 0
+            headsNumber = 0
+            document.getElementById('heads').innerHTML = 0
+            document.getElementById('tails').innerHTML = 0
+            document.getElementById('tails-percent').innerHTML = 0
+            document.getElementById('heads-percent').innerHTML = 0
+
+        })
+
+
+
+    })
+
+
 
     // TODO: Add event listener and handler for flip and clear buttons
 
